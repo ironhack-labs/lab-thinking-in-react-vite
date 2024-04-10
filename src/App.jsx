d8b9a3cb391cfe0ede74ff291e325c8fce61a692
Alejandro Martínez <alejandromartinezvarel@gmail.comgit config --global user.name Alejandro>
2024-04-10 13:48:11 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import ProductsPage from "./components/products-page/ProductPage";
 
 function App() {
+  
   return (
     <div className="App">
       <h1>React | Thinking in React</h1>
+      <ProductsPage />
     </div>
   );
 }
