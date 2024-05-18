55c2b494372079bc4e6e782c93fb855a63af34cf
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-05-18 15:42:24 +0100

    done

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -5,11 +5,45 @@ import ProductTable from "./ProductTable";
 
 function ProductsPage() {
   const [products, setProducts] = useState(jsonData);
+  const [search, setSearch] = useState("");
+  const [inStock, setInStock] = useState(false);
+
+  const handleSearch = value => {
+   
+    const filteredProducts = jsonData.filter(product => {
+      return product.name.toLowerCase().includes(value.toLowerCase())
+    });
+    setProducts(filteredProducts);
+
+    setSearch(value);
+
+  };
+
+
+
+   const handleInStock = event => {
+
+    if(event.target.checked) {
+
+      const filteredInStock = jsonData.filter(productDetails => {
+        return productDetails.inStock ? true : false
+      }) 
+      setProducts(filteredInStock) 
+
+    } else {
+
+      setProducts(jsonData) 
+    }
+
+
+    setInStock(event.target.checked)
+  } 
+
 
   return (
     <>
       <h1>IronStore</h1>
-      <SearchBar allProducts={products}/>
+      <SearchBar allProducts={products} searchValue={search} filterProducts={handleSearch} tickBoxValue={inStock} isInStock={handleInStock} />
       <ProductTable allProducts={products} />
     </>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,36 +1,9 @@
-import { useState } from "react";
-
 
 const SearchBar = (props) => {
 
-const [search, setSearch] = useState("");
-const [filter, setFilter] = useState(props.allProducts);
-const [inStock, setInStock] = useState(false)
-    
-const handleSearch = event => {
-     
-    setSearch(event.target.value);
-    const filteredProducts = props.allProducts.filter(product => (product.name.toLowerCase()).includes(search.toLowerCase()));
-    setFilter(filteredProducts);
-    console.log(filteredProducts) // sth is wrong with filtering - it filters only every second letter
 
-
-/* how to pass it to the table?? */
-/*     return (
-        filter.map(product => (
-            <tr key={product.id}>
-                <td>{product.name}</td>
-                <td>{product.price}</td>
-
-            </tr>
-        ))
-    ) */
-
-};
-
-const handleInStock = (event) => {
-    setInStock(event.target.checked);
-    props.allProducts.filter(productDetails => {return (productDetails.inStock ? productDetails : "")})
+const handleSearch = (event) => {
+    props.filterProducts(event.target.value)
 }
     
     return (
@@ -38,22 +11,14 @@ const handleInStock = (event) => {
             <>
             <form>
             <label>Search <br/></label>
-                <input className="search-window" type="text" name="searchedValue" value={search} onChange={handleSearch}/>
+                <input className="search-window" type="text" name="searchedValue" value={props.searchValue} onChange={handleSearch}/>
 
             </form>
 
             <form>
-                <input type="checkbox" name="inStock" checked={inStock} onChange={handleInStock}/>
+                <input type="checkbox" name="inStock" checked={props.tickBoxValue} onChange={props.isInStock}/>
                 <label> Only show products in stock </label>
             </form>
-
-                {filter.map(product => (
-                <tr key={product.id}>
-                    <td>{product.name}</td>
-                    <td>{product.price}</td>
-
-                </tr>
-                ))}
 
             </>
 
